9b01a20642e030f9ab55fac48cda22a2d3f8f264
GuptaJuluri22 <19a85a0222@sves.org.in>
2022-03-30 12:39:25 +0530

    added

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,10 @@
+
+
+/* To Generate the Bill Body*/
+void generateBillBody(char item[30],float qty,float price);
+
+
+/* To Generate the bill Header*/
+void generateBillHeader(char name[50],char date[30]);
+
+int count(int num);
---
+++ 3_Implementation/inc/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "function.h"
+
+
+int main(){
+
+if(count(2)==4){
+return 0;}
+
+else{
+return 1;}
+
+  
+}
