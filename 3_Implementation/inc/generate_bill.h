d296689f23a970ac4f4fea49b339939081e83fca
GuptaJuluri22 <101310082+GuptaJuluri22@users.noreply.github.com>
2022-04-03 16:20:37 +0530

    Add files via upload

---
+++ 3_Implementation/inc/generate_bill.h
@@ -0,0 +1,1 @@
+void generateBillFooter(float total);
---
+++ 3_Implementation/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "inc/function.h"
+
+
+
+
+int main(){
+
+
+if(count(2)==4){
+
+printf("No errors found");
+}
+
+return 0;
+
+
+
+
+
+
+
+}
